c81cddfda84ef85740f6233346207daa355f2123
maladaisy <maladaisy24@gmail.com>
2026-01-14 13:54:36 +0530

    Enhance middleware documentation and error handling in app.js

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ const {userAuth ,userAdmin, commonUser} = require("./src/utils/middleware");
 
 
 /* 
-// Note this is Global middle ware it runs for all the routes
+
+ NOTE:
+ This is a GLOBAL middleware.
+ It runs for ALL routes.
+
+ Examples:
+ - Calling /testing → this middleware runs
+ - Calling /userAuth → this middleware runs
+ - Calling any route → this middleware runs
+
+ This is NOT bad practice by default.
+ Global middleware is good for:
+ - logging
+ - body parsing
+ - security checks
+
+ But it is a BAD practice to use global middleware
+ for route-specific logic like authentication.
 app.use((req, res, next) => { 
     console.log("iam middleware");
     next(); 
@@ -12,6 +29,12 @@ app.use((req, res, next) => {
  so write it in down how i write code
 */
 
+/*
+
+Interview One-Liner (Very Important ⭐)
+Global middleware is good for common logic,
+but route-specific logic should be applied only to required routes.
+*/
 
 const logger = ((req, res, next) => { 
     console.log("iam middleware");
@@ -33,14 +56,54 @@ app.use("/AdminAuth", userAdmin, (req, res) => {
 })
 
 /* Another method of Writing Middleware */
-
-app.get("/common/userInfo", commonUser , (req , res ) => {
+ app.use("/common", commonUser);
+app.get("/common/userInfo" , (req , res ) => {
    res.send("Information fetched Successfully");
 });
 
-app.get("/common/deleteInfo", commonUser , (req,res) => {
+app.get("/common/deleteInfo" , (req,res) => {
    res.send("Deleted information Successfully");
 })
+
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        console.error("inside if",err);
+        return res.status(500).send("1st => Something went wrong !!!");
+    }
+   
+});
+
+// Error-handling middleware must accept 4 arguments: (err, req, res, next)
+app.use("/testing", (req, res) => {
+
+    try{
+      throw new Error("Basic error")
+    }catch{
+      
+      res.status(500).send("UnAuthorized Error !!");
+    }
+    
+});
+
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        console.error("inside if",err);
+        return res.status(500).send("last => Something went wrong !!!");
+    }
+});
+
+
+/*
+  Note :
+  test1 : /testing
+  if try catch there catch block execute and send response
+if catch and throw not there only throw present then error handling middleware will execute and send response
+    test2 : / => line 68 and 88 present
+      its takes bottom / error  last => Something went wrong !!!
+    test3 : / => line 68 and 88 not present
+        its takes Basic error from throw new Error("Basic error")
+*/
+
 
 
 app.listen(5000, () => {

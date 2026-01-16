b3a47880cb6dc2a86b4c78f86cfca12b67899812
maladaisy <maladaisy24@gmail.com>
2026-01-16 15:59:46 +0530

    Refactor user API endpoints for improved error handling and data management; update database connection string

--- main.js
+++ main.js
@@ -3,22 +3,89 @@ const Database = require("./src/config/database");
 const UserModels = require("./src/models/schema");
 const app = express();
 
+// this helps to convert json data to javascript object it  acts 
+// as middleware or plugin and this javascript object sends back to req.body
+app.use(express.json()); 
+
+// create
 app.post("/signUp", async (req,res) => {
-    let user = {
-      name: "Avinash",
-      age: 27,
-      email: "avi@gmail.com"
-    }
+  /* creating dynamic instance from the api through req */
+      //console.log("req Data", req.body);
+      let user = req.body
+    // Creating instance of User Model
+    // let user = {
+    //   name: "Meenu",
+    //   age: 27,
+    //   email: "kutti@gmail.com"
+    // }
 
     let userDetails = new UserModels(user);
     try{
       await userDetails.save();
     res.status(200).send("Saved Successfully");
     }catch(err){
-        res.status(400).send("Falied")
+        res.status(400).send("No Data Added !!")
     }
     
 });
+
+
+// feed Api => fetch all Data
+app.get("/getusers", async (req, res) => {
+    try{
+       let data = await UserModels.find({});
+       res.status(200).send(data);
+      
+    }catch{
+      res.status(400).send("Data Fetch is Falied !!")
+    }
+});
+
+// fetch each data based on the condition
+app.get("/getuserId", async (req, res) => {
+  //  let findId = req.body.userId;
+   let name = req.body.userName; // fetch by using name in post man send userName: "xyz"
+    try{
+
+       let data = await UserModels.find({ name });
+       res.status(200).send(data);
+      
+    }catch{
+      res.status(400).send("Data Fetch is Falied !!")
+    }
+});
+
+//deleting Api
+app.delete("/deleteUser", async (req, res) => {
+    let _id = req.body.userId; //_id => the name should match with schema _id data base having _id
+   
+    try{
+       await UserModels.findByIdAndDelete({ _id });
+       res.status(200).send("Deleted Successfully");
+      
+    }catch{
+      res.status(400).send("Data Fetch is Falied !!")
+    }
+});
+
+//Update Api
+app.patch("/updateUser", async (req, res) => {
+    let _id = req.body.userid; //_id => the name should match with schema _id data base having _id
+    let userUpdateData = req.body;
+    try{
+       let updated = await UserModels.findByIdAndUpdate(_id , userUpdateData , { new: true });
+       /* 
+       instead of return document replacing new: true / false
+       the new :true  return the updated data  
+       without new :false it will return old data
+        */
+       res.status(200).send("Data Updated Successfuly!!!");
+      
+    }catch{
+      res.status(400).send("Data Fetch is Falied !!")
+    }
+});
+
 
 Database().then(() => {
     console.log("mongoose is Connected");
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const Database = async () => {
-    await mongoose.connect("mongodb+srv://malachitti99_db_user:TWJPmRdDMB6IGpjD@sample-project.pz2lgqa.mongodb.net/DevTinder"); // it will create DevTinder in Db
+    await mongoose.connect("mongodb+srv://malachitti99_db_user:mala_daisy%402499@sample-project.pz2lgqa.mongodb.net/DevTinder"); // it will create DevTinder in Db
 }
 
 module.exports = Database;
